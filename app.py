f4d3a681fe0ceed8d09b2d92d60ed0fc896b28b1
singlachirag20 <119451029+singlachirag20@users.noreply.github.com>
2022-11-30 22:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,31 +11,39 @@ This app finds the maximum number from the given input
 
 st.header('User Input Parameters')
 
-def user_input_features():
-    firstNumber = st.number_input("First No")
-    secondNumber = st.number_input("Second No")
-    thirdNumber = st.number_input("Third No")
+firstNumber = st.number_input("First No")
+secondNumber = st.number_input("Second No")
+thirdNumber = st.number_input("Third No")
 
-    inputData = {'firstNumber': firstNumber,
-            'secondNumber': secondNumber,
-            'thirdNumber': thirdNumber
-            }
-    features = pd.DataFrame(inputData, index = [0])
-    return features
-
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df.to_dict())
-
-#Processing
 st.subheader('Result')
-
-firstNumber = df['firstNumber']
-secNumber = df['secondNumber']
-thirdNumber = df['thirdNumber']
-res = max(firstNumber,secNumber,thirdNumber)
+res = max(firstNumber,secondNumber,thirdNumber)
 st.write(res)
+
+# def user_input_features():
+#     firstNumber = st.number_input("First No")
+#     secondNumber = st.number_input("Second No")
+#     thirdNumber = st.number_input("Third No")
+
+#     inputData = {'firstNumber': firstNumber,
+#             'secondNumber': secondNumber,
+#             'thirdNumber': thirdNumber
+#             }
+#     features = pd.DataFrame(inputData, index = [0])
+#     return features
+
+# df = user_input_features()
+
+# st.subheader('User Input parameters')
+# st.write(df.to_dict())
+
+# #Processing
+# st.subheader('Result')
+
+# firstNumber = df['firstNumber']
+# secNumber = df['secondNumber']
+# thirdNumber = df['thirdNumber']
+# res = max(firstNumber,secNumber,thirdNumber)
+# st.write(res)
 # if ((df['firstNumber'] > df['secondNumber']) & (df['firstNumber'] > df['thirdNumber'])):
 #     st.write(df['firstNumber'])
 # elif ((df['secondNumber'] > df['firstNumber']) & (df['secondNumber'] > df['thirdNumber'])):
